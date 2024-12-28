3c12064d76149cd5cd0e41a3ae121fef75961abb
d_gggl <dimitri.gaggioli@gmail.com>
2024-12-28 17:56:28 +0100

    "Modifications models.py"

--- models.py
+++ models.py
@@ -1,13 +1,16 @@
 class Tournament:
-    def __init__(self, name, location, date, description):
-        self.name = name
-        self.location = location
-        self.date = date
-        self.description = description
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.location = kwargs['location']
+        self.date = kwargs['date']
+        self.description = kwargs.get('description', 'A chess tournament')
         self.rounds = []
 
-    def add_round(self, round):
-        self.rounds.append(round)
+    def add_round(self, chess_round):
+        if isinstance(chess_round, Round):
+            self.rounds.append(chess_round)
+        else:
+            raise TypeError("round must be of type Round")
 
 
 class Round:
@@ -16,13 +19,20 @@ class Round:
         self.matches = []
 
     def add_match(self, match):
-        self.matches.append(match)
+        if isinstance(match, Match):
+            self.matches.append(match)
+        else:
+            raise TypeError("match must be of type Match")
 
 
 class Match:
     def __init__(self, player1, player2, score1=0, score2=0):
-        self.player1 = player1
-        self.player2 = player2
+        if isinstance(player1, Player) and isinstance(player2, Player):
+            self.player1 = player1
+            self.player2 = player2
+        else:
+            raise TypeError("players must be of type Player")
+
         self.score1 = score1
         self.score2 = score2
 
@@ -30,11 +40,12 @@ class Match:
         self.score1 = score1
         self.score2 = score2
 
+
 class Player:
-    def __init__(self, name, rank, national_id):
-        self.name = name
-        self.rank = rank
-        self.national_id = national_id
+    def __init__(self, **kwargs):
+        self.name = kwargs['name']
+        self.rank = kwargs['rank']
+        self.national_id = kwargs['national_id']
 
     def update_rank(self, new_rank):
-        self.rank = new_rank
+        self.rank['rank'] = new_rank
